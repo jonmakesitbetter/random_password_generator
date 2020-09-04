2ccc2d50a35923512304c8f2fdb4ec3c7666d4af
unknown <jonmakesitbetter@gmail.com>
2020-09-03 20:58:06 -0400

    Cleaned code, DRY, added if clause to account for no character arrays being selected by the user

--- Develop/script.js
+++ Develop/script.js
@@ -8,126 +8,10 @@ function writePassword() {
 
   passwordText.value = password;
 }
-var upperCaseArray = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-var lowerCaseArray = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
-var numberArray = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  0,
-  1,
-  2,
-  3,
-  4,
-  5,
-  6,
-  7,
-  8,
-  9,
-  0,
-];
-var characterArray = [
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+var numberArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,];
+var characterArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")",];
 var passwordOptions = [];
 function generatePassword() {
   var password = [];
@@ -136,37 +20,37 @@ function generatePassword() {
   );
   if (length <= 8 || length >= 128) {
     alert("Your password must be between 8 and 128 characters long.");
-  } else generatePassword();
+    writePassword();
+  } 
 
-  console.log(length);
-  var letters = confirm("Do you want your password to contain letters?");
-  if (letters === true) {
-    var passwordLetters =
-      lowerCaseArray[Math.floor(Math.random() * password.length)];
-    password.push(passwordLetters);
-    console.log(password);
-  }
+  var capitalLetters = confirm("Do you want your password to contain capital letters?");
+  var lowerCaseLetters = confirm("Do you want your password to contain lower case letters?");
   var numbers = confirm("Do you want your password to contain numbers?");
-  if (numbers === true) {
-    var passwordNumbers =
-      numberArray[Math.floor(Math.random() * password.length)];
-    password.push(passwordNumbers);
-    console.log(password);
-  }
-  var specialCharacters = confirm(
-    "Do you want your password to contain special characters?"
-  );
-  if (specialCharacters === true) {
-    var passwordCharacters =
-      characterArray[Math.floor(Math.random() * password.length)];
-    password.push(passwordCharacters);
-    console.log(password);
-  }
+  var specialCharacters = confirm("Do you want your password to contain special characters?");
 
+  if (capitalLetters === true) {
+      passwordOptions.push(upperCaseArray);
+  }
+  if (lowerCaseLetters === true) {
+      passwordOptions.push(lowerCaseArray);
+}
+  if (numbers === true) {
+      passwordOptions.push(numberArray);
+  }
+  if (specialCharacters === true) {
+      passwordOptions.push(characterArray);
+
+      if (capitalLetters !== true && lowerCaseLetters !== true && numbers !== true && specialCharacters !== true){
+        alert("Your password must contain characters!");
+        writePassword();
+      }
+ console.log(passwordOptions);
+  }
+  //lowerCaseArray[Math.floor(Math.random() * password.length)]; === example code for randomizer
 
 
   return password.join("");
-
+}
   // Add event listener to generate button
 
   // ALL YOUR CODE GOES HERE
@@ -178,32 +62,25 @@ function generatePassword() {
   // WHEN prompted for the length of the password
   // THEN I choose a length of at least 8 characters and no more than 128 characters
   // WHEN prompted for character types to include in the password
-  //          Need confirm for each type - number, character, and letter, to contain only true boolean response type.
-  //          confirm.character("Do you want your password to contain special characters?") confirm.letters() confirm.numbers() End in an else { "Your password must contain something!"" and return to original prompt };
   // THEN I choose lowercase, uppercase, numeric, and/or special characters
-  //          Already handled
   // WHEN I answer each prompt
   // THEN my input should be validated and at least one character type should be selected
-  //          Also already handled
   // WHEN all prompts are answered
   // THEN a password is generated that matches the selected criteria
   // WHEN the password is generated
   // THEN the password is either displayed in an alert or written to the page
-  //          Sounds good.
-}
+
 
 generateBtn.addEventListener("click", writePassword);
 
-if (tagName !== "h1" && tagName !== "h2" && tagName !== "p" && tagName !== "div") {
-  alert("please enter a valid tag");
-} else {
-  var tag = document.createElement(tagName);
-  tag.textContent = "This was made via prompts. It's a " + tagName;
-  document.body.appendChild(tag);
+// if (tagName !== "h1" && tagName !== "h2" && tagName !== "p" && tagName !== "div") {
+//   alert("please enter a valid tag");
+// } else {
+//   var tag = document.createElement(tagName);
+//   tag.textContent = "This was made via prompts. It's a " + tagName;
+//   document.body.appendChild(tag);
 
 
-  for (var i = 0; i < length i++){
-    if (i == password.length()){
+//   for (var i = 0; i < length i++){
+//     if (i == password.length()){
       //THEN STOP DOING THAT
-    }
-  }
